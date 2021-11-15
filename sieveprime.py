b2f5a24de2518a690e0cd427cbdc0378f0b926e2
evelyn <evelyngon@gmail.com>
2021-11-15 04:05:05 -0500

    firts commit algorithm Sieve of Eratosthenes

---
+++ sieveprime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+
+#algorithm Sieve of Eratosthenes
+
+def inquire_number():
+    number = int (input("Hey!, enter an integer number n>1? "))
+    return number
+
+def sieveprime(n):
+    notprimes = []
+    for i in range(2, n+1):
+        if i not in notprimes:
+            print (i)
+            for j in range(i*i, n+1, i):
+                notprimes.append(j)
+
+if __name__=="__main__":
+    number = inquire_number()
+    sieveprime(number)
